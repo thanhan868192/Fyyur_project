c33a073bdb75ac1a0c72e1be25132bbaad958e72
MSI\Admin <thanhan.lan@gmail.com>
2022-07-20 01:34:02 +0700

    remove commd line

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -7,6 +7,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 # Connect to the database
 
- #add this line
 # TODO IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
